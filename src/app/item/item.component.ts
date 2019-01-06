6be53314a0a8b1d0ad63cb1984476e7a9a371130
Lord Loc <loc.truong@upskills.com>
2019-01-06 20:49:45 +0700

    updates to deploy to heroku

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import Quagga from 'quagga';
 import { Item } from '../order/order.component';
+import { Barcode, BarcodePicker, ScanSettings, configure } from "scandit-sdk";
+import { API_KEY } from '../app.constants';
 
 @Component({
   selector: 'app-item',
@@ -15,30 +17,60 @@ export class ItemComponent implements OnInit {
 
   ngOnInit() {
     this.item = new Item(undefined)
-
-    Quagga.init({
-      inputStream: {
-        name: "Live",
-        type: "LiveStream"
-      },
-      decoder: {
-        readers: ["upc_e_reader", "upc_reader"]
-      }
-    }, function () {
-      console.log("Initialization finished. Ready to start");
-      Quagga.start();
+    configure(API_KEY, {
+      engineLocation: "../../../assets/build"
     });
+    BarcodePicker.create(document.getElementById("scandit-barcode-picker"), {
+      playSoundOnScan: true,
+      vibrateOnScan: true
+    }).then((barcodePicker) => {
+      // barcodePicker is ready here to be used (rest of the tutorial code should go here)
+      const scanSettings = new ScanSettings({
+        enabledSymbologies: [
+          Barcode.Symbology.EAN8,
+          Barcode.Symbology.EAN13,
+          Barcode.Symbology.UPCA,
+          Barcode.Symbology.UPCE,
+          Barcode.Symbology.CODE128,
+          Barcode.Symbology.CODE39,
+          Barcode.Symbology.CODE93,
+          Barcode.Symbology.INTERLEAVED_2_OF_5
+        ],
+        codeDuplicateFilter: 1000
+      });
+      barcodePicker.applyScanSettings(scanSettings);
+      barcodePicker.onScan((scanResult) => {
+        scanResult.barcodes.reduce((string, barcode) => {
+          console.log( string + `${Barcode.Symbology.toHumanizedName(barcode.symbology)}: ${barcode.data}\n`)
+          this.item.code = barcode.data
+          return string + `${Barcode.Symbology.toHumanizedName(barcode.symbology)}: ${barcode.data}\n`
+        }, "");
+      });
 
-    Quagga.onDetected(data => {
-      let detectedCode = data.codeResult.code
-      if (detectedCode != null && detectedCode != '' && detectedCode != undefined) {
-        if(this.item.code == undefined) {
-          this.item.code = detectedCode
-          console.log("Detected ",detectedCode, "!");
-          this.playAudio()
-        }
-      }
-    })
+    });
+    // Quagga.init({
+    //   inputStream: {
+    //     name: "Live",
+    //     type: "LiveStream"
+    //   },
+    //   decoder: {
+    //     readers: ["ean_reader"]
+    //   }
+    // }, function () {
+    //   console.log("Initialization finished. Ready to start");
+    //   Quagga.start();
+    // });
+
+    // Quagga.onDetected(data => {
+    //   let detectedCode = data.codeResult.code
+    //   if (detectedCode != null && detectedCode != '' && detectedCode != undefined) {
+    //     if(this.item.code == undefined) {
+    //       this.item.code = detectedCode
+    //       console.log("Detected ",detectedCode, "!");
+    //       this.playAudio()
+    //     }
+    //   }
+    // })
   }
 
   public closeDialog() {
