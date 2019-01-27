3ac5060e9305ebde9293e4d4ef3af8d648d74508
Lord Loc <loc.truong@upskills.com>
2019-01-27 16:45:49 +0700

    [C] - forgot to change api base url.

--- src/app/service/data/customer.service.ts
+++ src/app/service/data/customer.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { API_URL } from 'src/app/app.constants';
+import { API_URL, API_URL_PROD } from 'src/app/app.constants';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,6 @@ export class CustomerService {
   constructor(private http:HttpClient) { }
 
   retrieveAllCustomers() {
-    return this.http.get<Array<any>>(`${API_URL}/api/customer`)
+    return this.http.get<Array<any>>(`${API_URL_PROD}/api/customer`)
   }
 }
